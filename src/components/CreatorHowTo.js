9c96b6d415b8fb4b0892063b381e33b44e28b9d3
Gina Beth Russell <ginabethrussell@Ginas-MBP.cinci.rr.com>
2021-01-29 10:33:23 -0500

    add Card styles to CreatorHowTo.js

--- src/components/CreatorHowTo.js
+++ src/components/CreatorHowTo.js
@@ -1,21 +1,34 @@
 import React from 'react'
 import {Button, ButtonGroup} from 'reactstrap';
+import {
+    Card, CardText, CardBody,
+    CardTitle, CardSubtitle, CardHeader, CardFooter
+  } from 'reactstrap';
+
 // displays individual howto
 function CreatorHowTo({howto, handleEdit, handleDelete}) {
 
     return (
         <div className='creator-howto-wrapper'>
-            <div className='creator-buttons'>
-                <ButtonGroup size='lg'>
-                    <Button color='info' onClick={() => handleEdit(howto.id)}>Edit</Button>
-                    <Button color='info' onClick={() => handleDelete(howto.id)}>Delete</Button>
-                </ButtonGroup>
-            </div>
-            <h3>{howto.title}</h3>
-            <h5>{howto.author}</h5>
-            {howto.paragraphs.map(paragraph => (
-                <p>{paragraph}</p>
-            ))}
+            <Card>
+                <CardHeader> 
+                    <CardTitle>{howto.title}</CardTitle>
+                    <CardSubtitle>{howto.author}</CardSubtitle>
+                </CardHeader>
+                <CardBody>
+                    {howto.paragraphs.map(paragraph => (
+                        <CardText>{paragraph}</CardText>
+                    ))}
+                </CardBody>
+                <CardFooter>
+                    <div className='creator-buttons'>
+                        <ButtonGroup size='lg'>
+                            <Button color='info' onClick={() => handleEdit(howto.id)}>Edit</Button>
+                            <Button color='info' onClick={() => handleDelete(howto.id)}>Delete</Button>
+                        </ButtonGroup>
+                    </div>
+                </CardFooter>
+            </Card>
         </div>
     )
 }
